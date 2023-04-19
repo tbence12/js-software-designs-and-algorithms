d5de45e41bf25d8e80f537bbe1404c9b83a0a1f4
T. Bence <turi.b12@gmail.com>
2023-04-19 23:57:39 +0200

    Task_7-Algorithms-and-data-structures-part-2

---
+++ 7-Algorithms-and-data-structures-part-2/JobRunner.ts
@@ -0,0 +1,73 @@
+class Job {
+  name: string;
+  priority: number;
+
+  constructor(name: string, priority: number) {
+    this.name = name;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  private queue: Job[];
+
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(job: Job): void {
+    if (this.isEmpty()) {
+      this.queue.push(job);
+    } else {
+      let added = false;
+      for (let i = 0; i < this.queue.length; i++) {
+        if (job.priority < this.queue[i].priority) {
+          this.queue.splice(i, 0, job);
+          added = true;
+          break;
+        }
+      }
+      if (!added) {
+        this.queue.push(job);
+      }
+    }
+  }
+
+  dequeue(): Job | undefined {
+    return this.queue.shift();
+  }
+
+  isEmpty(): boolean {
+    return this.queue.length === 0;
+  }
+}
+
+class JobRunner {
+  private queue: PriorityQueue;
+
+  constructor() {
+    this.queue = new PriorityQueue();
+  }
+
+  addJob(job: Job): void {
+    this.queue.enqueue(job);
+  }
+
+  runJobs(): void {
+    while (!this.queue.isEmpty()) {
+      const job = this.queue.dequeue();
+      console.log(`Running job "${job?.name}"...`);
+    }
+  }
+}
+
+const job1 = new Job("Job 1", 3);
+const job2 = new Job("Job 2", 1);
+const job3 = new Job("Job 3", 2);
+
+const jobRunner = new JobRunner();
+jobRunner.addJob(job1);
+jobRunner.addJob(job2);
+jobRunner.addJob(job3);
+
+jobRunner.runJobs();
